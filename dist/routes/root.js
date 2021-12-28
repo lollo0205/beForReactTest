3be69476e6aee29b25094ee9d617dde554bd259d
beltramol <loris.beltramo@dncsrl.com>
2021-12-28 14:06:28 +0100

    fix error typescript and add dist

--- dist/routes/root.js
+++ dist/routes/root.js
@@ -2,7 +2,6 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const root = async (fastify, opts) => {
     fastify.get('/', async function (request, reply) {
-        console.log();
         return { root: true };
     });
 };
--- src/routes/root.ts
+++ src/routes/root.ts
@@ -2,7 +2,6 @@ import { FastifyPluginAsync } from 'fastify'
 
 const root: FastifyPluginAsync = async (fastify, opts): Promise<void> => {
   fastify.get('/', async function (request, reply) {
-    console.log();
     return { root: true }
   })
 }
